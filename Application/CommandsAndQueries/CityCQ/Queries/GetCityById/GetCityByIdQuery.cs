fcdfd4730d9cb3ef32c5ca05204bf275202dd965
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 21:49:45 +0300

    Implement city get by id query

---
+++ Application/CommandsAndQueries/CityCQ/Queries/GetCityById/GetCityByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dtos.CityDtos;
+using MediatR;
+
+namespace Application.CommandsAndQueries.CityCQ.Query.GetCityById
+{
+    public class GetCityByIdQuery(int cityId) : IRequest<CityDto>
+    {
+        public int CityId { get; } = cityId;
+    }
+}
