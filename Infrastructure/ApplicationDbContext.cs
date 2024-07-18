3d97c1bef130e056f60bba78ab9134fc00b607d7
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 17:42:50 +0300

    Create Configuration for amebity

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,10 +1,17 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Infrastructure
 {
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Amenity> Amenities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
