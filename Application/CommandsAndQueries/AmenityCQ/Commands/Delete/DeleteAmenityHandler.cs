6016707eba0ce2bfd49f704bc93fcc2ef972c307
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 19:03:46 +0300

    Implement delete amenity command

---
+++ Application/CommandsAndQueries/AmenityCQ/Commands/Delete/DeleteAmenityHandler.cs
@@ -0,0 +1,47 @@
+﻿using Application.Dtos.AmenityDtos;
+using Application.Exceptions;
+using Application.Execptions;
+using AutoMapper;
+using Domain.Abstractions;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.CommandsAndQueries.AmenityCQ.Commands.Delete
+{
+    public class DeleteAmenityHandler : IRequestHandler<DeleteAmenityCommand, AmenityDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IAmenityRepository _repository;
+
+        public DeleteAmenityHandler(IAmenityRepository repository)
+        {
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Amenity, AmenityDto>();
+                cfg.CreateMap<AmenityDto, Amenity>();
+            });
+            _mapper = configuration.CreateMapper();
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public async Task<AmenityDto> Handle(DeleteAmenityCommand request, CancellationToken cancellationToken)
+        {
+            Amenity deletedAmenity;
+            try
+            {
+                var amenity = await _repository.GetByIdAsync(request.Id);
+                if (amenity is null) throw new NotFoundException();
+                deletedAmenity = await _repository.DeleteAsync(amenity);
+            }
+            catch (NotFoundException) {
+                throw;
+            }
+            catch (Exception)
+            {
+
+                throw new ErrorException($"Error during deleting amenity with id '{request.Id}'.");
+            }
+            return _mapper.Map<AmenityDto>(deletedAmenity);
+        }
+    }
+}
