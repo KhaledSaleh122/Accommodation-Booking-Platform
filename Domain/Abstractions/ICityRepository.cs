a08ef80783edb25538efced84680e66ad8803669
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 21:02:09 +0300

    Create interface for city repository

---
+++ Domain/Abstractions/ICityRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Abstractions
+{
+    public interface ICityRepository
+    {
+        Task<bool> CheckPostOfficeExistsAsync(string postOffice);
+        Task<City> DeleteAsync(City city);
+        public Task<(IEnumerable<City>, int)> GetAsync(int page, int pageSize, string? country);
+        Task<City?> GetByIdAsync(int cityId);
+        Task CreateAsync(City city);
+        Task UpdateAsync(City updatedCity);
+    }
+}
