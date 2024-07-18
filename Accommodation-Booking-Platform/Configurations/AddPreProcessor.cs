96de289aa7b2c2231db9dbb537916921be4bc5a1
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 18:44:04 +0300

    Create generic Register pre processor service, to register pre processors

---
+++ Accommodation-Booking-Platform/Configurations/AddPreProcessor.cs
@@ -0,0 +1,13 @@
+﻿using Application.Validation;
+using MediatR.Pipeline;
+
+namespace Booking_API_Project.Configurations
+{
+    public static class AddPreProcessor
+    {
+        public static MediatRServiceConfiguration RegisterPreProcessor<TRequest>(this MediatRServiceConfiguration services) where TRequest : notnull {
+            services.AddRequestPreProcessor<IRequestPreProcessor<TRequest>, ValidationProcessor<TRequest>>();
+            return services;
+        }
+    }
+}
