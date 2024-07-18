20dd7f560d9746e93fa4321e73845572d8681bd8
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 19:36:08 +0300

    Create custom validation response

---
+++ Presentation/Responses/Validation/ValidationFailureResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Presentation.Responses.Validation
+{
+    public class ValidationFailureResponse
+    {
+        public List<ValidationResponse> Errors { get; set; }
+        public string Title { get; set; }
+
+        public int Status { get; set; }
+
+        public string TraceId { get; set; }
+    }
+}
