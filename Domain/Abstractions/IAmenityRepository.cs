1416c511f419a88bdb2952635f89f2c5e624aee1
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 17:32:06 +0300

    Create interface for the amenity repository

---
+++ Domain/Abstractions/IAmenityRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Domain.Abstractions
+{
+    public interface IAmenityRepository
+    {
+        public Task CreateAsync(Amenity amenity);
+        public Task<Amenity?> GetByIdAsync(int id);
+
+        public Task<(IEnumerable<Amenity>,int)> GetAsync(int page, int pageSize);
+
+        public Task<Amenity> DeleteAsync(Amenity amenity);
+
+        public Task UpdateAsync(Amenity amenity);
+
+    }
+}
