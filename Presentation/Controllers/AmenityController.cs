91bede3738748e8efc9b2eeb26d7b07658e2eebc
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 19:41:33 +0300

    Implement create amenity action

--- Presentation/Controllers/AmenityController.cs
+++ Presentation/Controllers/AmenityController.cs
@@ -1,10 +1,13 @@
-﻿using Application.CommandsAndQueries.AmenityCQ.Query.GetAmenityById;
+﻿using Application.CommandsAndQueries.AmenityCQ.Commands.Create;
+using Application.CommandsAndQueries.AmenityCQ.Query.GetAmenityById;
 using Application.Dtos.AmenityDtos;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Presentation.Responses.NotFound;
+using Presentation.Responses.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Presentation.Controllers
 {
@@ -21,6 +24,26 @@ namespace Presentation.Controllers
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
+        [HttpPost]
+        [ProducesResponseType(typeof(AmenityDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ValidationFailureResponse), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateAmenity([Required] CreateAmenityCommand request)
+        {
+            var amenityDto = await _mediator.Send(request);
+            _logger.LogInformation
+            (
+                "New amenity created: Id={amenityDto.Id}, Name={amenityDto.Name}," +
+                " Description={amenityDto.Description}",
+                amenityDto.Id,
+                amenityDto.Name,
+                amenityDto.Description
+            );
+            return CreatedAtAction(
+                nameof(GetAmenity),
+                new { amenityId = amenityDto.Id },
+                amenityDto
+            );
+        }
 
         [HttpGet("{amenityId}")]
         [ProducesResponseType(typeof(AmenityDto), StatusCodes.Status200OK)]
