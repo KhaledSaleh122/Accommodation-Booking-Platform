a76085912947b44f6afb5dcca1ad1d0be9412575
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 17:56:20 +0300

    Create migration for create amenity table

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -8,6 +8,10 @@ namespace Infrastructure
     {
         public DbSet<Amenity> Amenities { get; set; }
 
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
