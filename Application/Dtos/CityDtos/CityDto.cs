a222693e28affa14d0993f60a029c49fa26c339b
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 21:47:06 +0300

    Create City Dto

---
+++ Application/Dtos/CityDtos/CityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Dtos.CityDtos
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public string PostOffice { get; set; }
+    }
+}
