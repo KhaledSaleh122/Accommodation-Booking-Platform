cb874aeff6272d10038defff2384d6dba6db3220
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 17:49:26 +0300

    Add required Configuartion to connect to sql server

---
+++ Accommodation Booking Platform/Configurations/AddDBcontextSetup.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking_API_Project.Configurations
+{
+    public static class AddDBcontextSetup
+    {
+        public static IServiceCollection AddDBContextConfiguration(this IServiceCollection services, IConfiguration configuration) {
+            services.AddDbContext<ApplicationDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                },
+                ServiceLifetime.Scoped
+            );
+            return services;
+        }
+    }
+}
--- Accommodation Booking Platform/Program.cs
+++ Accommodation Booking Platform/Program.cs
@@ -1,8 +1,12 @@
+using Booking_API_Project.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDBContextConfiguration(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
