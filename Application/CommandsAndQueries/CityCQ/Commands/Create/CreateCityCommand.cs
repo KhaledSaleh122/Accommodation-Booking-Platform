0a0f797d2565ea384814a1ef1e7804472703c08f
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 22:50:57 +0300

    Implement create city command

---
+++ Application/CommandsAndQueries/CityCQ/Commands/Create/CreateCityCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos.CityDtos;
+using MediatR;
+
+namespace Application.CommandsAndQueries.CityCQ.Commands.Create
+{
+    public class CreateCityCommand(string name, string country, string postOffice) : IRequest<CityDto>
+    {
+        public string Name { get; set; } = name;
+        public string Country { get; set; } = country;
+
+        public string PostOffice { get; set; } = postOffice;
+    }
+}
