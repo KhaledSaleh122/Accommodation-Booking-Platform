1b198ffe67c7e169baf0be293ecd2625cb836308
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 21:30:39 +0300

    Generate migration to create city table

---
+++ Infrastructure/Migrations/20240718182925_CreateCityTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCityTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PostOffice = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Name_Country",
+                table: "Cities",
+                columns: new[] { "Name", "Country" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_PostOffice",
+                table: "Cities",
+                column: "PostOffice",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
