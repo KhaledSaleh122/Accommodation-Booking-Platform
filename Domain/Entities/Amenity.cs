db28059a1c7c504c72cf389ca739cfb65983f7c5
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 17:29:55 +0300

    Implement the entity Amenity

---
+++ Domain/Entities/Amenity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+#nullable disable
+    public sealed class Amenity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
