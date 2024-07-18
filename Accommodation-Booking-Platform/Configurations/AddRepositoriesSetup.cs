694d98e646a69807d355df535786b6d541338099
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 18:54:05 +0300

    Add the amenity repository to IoC container

---
+++ Accommodation-Booking-Platform/Configurations/AddRepositoriesSetup.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Abstractions;
+using Infrastructure.Repositories;
+
+namespace Booking_API_Project.Configurations
+{
+    public static class AddRepositoriesSetup
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAmenityRepository, AmenityRepository>();
+            return services;
+        }
+    }
+}
--- Accommodation-Booking-Platform/Program.cs
+++ Accommodation-Booking-Platform/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddRepositories();
 builder.Services.AddDBContextConfiguration(builder.Configuration);
 builder.Services.AddValidatorsFromAssembly(Assembly.Load("Application"));
 
