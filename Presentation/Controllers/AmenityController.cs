a4cab3728e2fc41dbb0837d160543e3e14998388
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 19:19:26 +0300

    Implement get amenity action

---
+++ Presentation/Controllers/AmenityController.cs
@@ -0,0 +1,34 @@
+﻿using Application.CommandsAndQueries.AmenityCQ.Query.GetAmenityById;
+using Application.Dtos.AmenityDtos;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Presentation.Controllers
+{
+
+    [ApiController]
+    [Route("api/amenities")]
+    public class AmenityController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<AmenityController> _logger;
+        public AmenityController(IMediator mediator, ILogger<AmenityController> logger)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+        [HttpGet("{amenityId}")]
+        [ProducesResponseType(typeof(AmenityDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult<AmenityDto>> GetAmenity(int amenityId)
+        {
+            var query = new GetAmenityByIdQuery(amenityId);
+            var amenityDto = await _mediator.Send(query);
+            if (amenityDto is null) return NotFound();
+            return Ok(amenityDto);
+        }
+    }
+}
