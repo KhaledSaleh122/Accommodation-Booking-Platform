f22951e10d55b8cc6ae49afee4cd12827848db58
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 18:12:22 +0300

    Implement create amenity command

---
+++ Application/CommandsAndQueries/AmenityCQ/Commands/Create/CreateAmenityCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dtos.AmenityDtos;
+using MediatR;
+namespace Application.CommandsAndQueries.AmenityCQ.Commands.Create
+{
+    public class CreateAmenityCommand(string name, string description) : IRequest<AmenityDto>
+    {
+
+        public string Name { get; set; } = name;
+        public string Description { get; set; } = description;
+    }
+}
