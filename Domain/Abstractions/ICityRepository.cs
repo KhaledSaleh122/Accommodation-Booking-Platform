3ac66c1c199beb2d86e0a7b16ed321385ce2a0d8
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 21:35:01 +0300

    Add method to check if a city exist in a country, to make sure no duplication

--- Domain/Abstractions/ICityRepository.cs
+++ Domain/Abstractions/ICityRepository.cs
@@ -4,7 +4,8 @@ namespace Domain.Abstractions
 {
     public interface ICityRepository
     {
-        Task<bool> CheckPostOfficeExistsAsync(string postOffice);
+        Task<bool> DoesPostOfficeExistsAsync(string postOffice);
+        Task<bool> DoesCityExistInCountryAsync(string cityName,string countryName);
         Task<City> DeleteAsync(City city);
         public Task<(IEnumerable<City>, int)> GetAsync(int page, int pageSize, string? country);
         Task<City?> GetByIdAsync(int cityId);
