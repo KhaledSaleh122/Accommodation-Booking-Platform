db83210048287883bdfb1830fcda10e5c2fae1be
khaled Saleh <voxagolde2003@gmail.com>
2024-07-18 19:03:46 +0300

    Implement delete amenity command

---
+++ Application/CommandsAndQueries/AmenityCQ/Commands/Delete/DeleteAmenityCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dtos.AmenityDtos;
+using MediatR;
+
+namespace Application.CommandsAndQueries.AmenityCQ.Commands.Delete
+{
+    public class DeleteAmenityCommand(int id) : IRequest<AmenityDto>
+    {
+        public int Id { get; set; } = id;
+    }
+}
