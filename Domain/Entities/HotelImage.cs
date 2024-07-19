e0baea3134582da756dee1fa2942c26a67319b7e
khaled Saleh <voxagolde2003@gmail.com>
2024-07-19 20:28:08 +0300

    Implement hotel Image entity

---
+++ Domain/Entities/HotelImage.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class HotelImage
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
