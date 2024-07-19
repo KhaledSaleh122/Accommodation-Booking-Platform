64bc2ee33b2da97fc7e2948afa88dc0baf1917b8
khaled Saleh <voxagolde2003@gmail.com>
2024-07-19 20:16:38 +0300

    Implement hotel types enum

---
+++ Domain/Entities/Hotel.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public sealed class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+
+        public string Owner { get; set; }
+        public string Address { get; set; }
+
+        public decimal PricePerNight { get; set; }
+        public HotelType HotelType { get; set; }
+
+        public int CityId { get; set; }
+        public City City { get; set; }
+        public ICollection<HotelImage> Images { get; set; }
+        public ICollection<Room> Rooms { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+
+        public ICollection<AmenityHotel> HotelAmenity { get; set; }
+
+    }
+}
