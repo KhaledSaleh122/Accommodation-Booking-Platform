0ed5cb800cdcf16987a9b0004c5c1136568d4c7a
khaled Saleh <voxagolde2003@gmail.com>
2024-07-19 20:46:20 +0300

    Add configuration for hotel amenity table

---
+++ Infrastructure/Configurations/HotelAmenityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    public class HotelAmenityConfiguration : IEntityTypeConfiguration<HotelAmenity>
+    {
+        public void Configure(EntityTypeBuilder<HotelAmenity> builder)
+        {
+            builder.HasKey(ha => new { ha.HotelId, ha.AmenityId });
+            builder.ToTable("HotelAmenity");
+        }
+    }
+}
