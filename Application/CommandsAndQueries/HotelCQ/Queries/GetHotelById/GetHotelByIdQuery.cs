2eee26dd861a47afe5631cc969308c4c5a276d60
khaled Saleh <voxagolde2003@gmail.com>
2024-07-19 21:15:30 +0300

    Create query get hotel by id

---
+++ Application/CommandsAndQueries/HotelCQ/Queries/GetHotelById/GetHotelByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using Application.Dtos.HotelDtos;
+using MediatR;
+
+namespace Application.CommandsAndQueries.HotelCQ.Query.GetHotelById
+{
+    public class GetHotelByIdQuery : IRequest<HotelFullDto?>
+    {
+        public GetHotelByIdQuery(int hotelId)
+        {
+            HotelId = hotelId;
+        }
+
+        public GetHotelByIdQuery() { 
+        
+        }
+
+        public int HotelId { get; }
+    }
+}
