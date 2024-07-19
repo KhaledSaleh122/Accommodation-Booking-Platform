92dc3180d092eba1431045197f98ac4502089b40
khaled Saleh <voxagolde2003@gmail.com>
2024-07-19 21:31:47 +0300

    Write test, to test get hotel by id when hotel not exist

---
+++ ABPIntegrationTests/HotelTests/HotelControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Application.Dtos.HotelDtos;
+using FluentAssertions;
+using Presentation.Responses.NotFound;
+using System.Net.Http.Json;
+using System.Net;
+
+namespace ABPIntegrationTests.HotelTests
+{
+    [TestCaseOrderer(
+    ordererTypeName: "ABPIntegrationTests.PriorityOrderer",
+    ordererAssemblyName: "ABPIntegrationTests")]
+    public class HotelControllerTests : IClassFixture<ABPWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+        private readonly static HotelDto _hotel = new();
+        private readonly string skipMessage = "Skipping due to create hotel test failure";
+        public HotelControllerTests(ABPWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Theory, TestPriority(2)]
+        [MemberData(nameof(HotelTestData.NotFoundTestData), MemberType = typeof(HotelTestData))]
+        public async Task GetHotel_ReturnsNotFound_WhenHotelNotExists(object id)
+        {
+            // Act
+            var response = await _client.GetAsync($"/api/hotels/{id}");
+            var notFound = await response.Content.ReadFromJsonAsync<NotFoundResponse>();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            notFound.Should().NotBeNull();
+        }
+    }
+}
---
+++ ABPIntegrationTests/HotelTests/HotelTestData.cs
@@ -0,0 +1,14 @@
+﻿namespace ABPIntegrationTests.HotelTests
+{
+    public static class HotelTestData
+    {
+        public static IEnumerable<object[]> NotFoundTestData =>
+            new List<object[]>
+            {
+                new object[] { 99999 },
+                new object[] { -1 },
+                new object[] { int.MaxValue },
+                new object[] { "test" },
+            };
+    }
+}
