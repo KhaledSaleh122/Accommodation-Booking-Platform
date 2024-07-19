4acccae341f1f95b2b69cde4c5daa65bf79d7d3c
khaled Saleh <voxagolde2003@gmail.com>
2024-07-19 21:00:55 +0300

    Implement Required dtos for hotel

---
+++ Application/Dtos/HotelDtos/HotelMinDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Dtos.HotelDtos
+{
+    public class HotelMinDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<string> Images { get; set; }
+        public string Thumbnail { get; set; }
+
+        public string Owner { get; set; }
+        public string Address { get; set; }
+        public string HotelType { get; set; }
+
+        public decimal PricePerNight { get; set; }
+    }
+}
---
+++ Application/Dtos/HotelDtos/HotelMinNoImagesDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Dtos.HotelDtos
+{
+    public class HotelMinNoImagesDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Thumbnail { get; set; }
+
+        public string Owner { get; set; }
+        public string Address { get; set; }
+        public string HotelType { get; set; }
+
+        public decimal PricePerNight { get; set; }
+    }
+}
