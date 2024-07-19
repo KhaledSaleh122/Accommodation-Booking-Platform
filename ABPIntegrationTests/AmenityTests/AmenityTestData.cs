a10c68ea4ff542e0cccb9b67db694cad4ee359f6
khaled Saleh <voxagolde2003@gmail.com>
2024-07-19 16:30:13 +0300

    Add more tests for amenity

--- ABPIntegrationTests/AmenityTests/AmenityTestData.cs
+++ ABPIntegrationTests/AmenityTests/AmenityTestData.cs
@@ -5,22 +5,36 @@ namespace ABPIntegrationTests.AmenityTests
 {
     public static class AmenityTestData
     {
-        public static IEnumerable<object?[]> GetCreateAmenityCommandData()
+        public static IEnumerable<object?[]> CreateAmenityTestData()
         {
             yield return new object?[] { null };
             yield return new object[] { new CreateAmenityCommand { Name = "test" } };
             yield return new object[] { new CreateAmenityCommand { Description = "test" } };
-            yield return new object[] { new CreateAmenityCommand { Description = "test" } };
+            yield return new object[] { new CreateAmenityCommand { Description = "", Name = "test" } };
+            yield return new object[] { new CreateAmenityCommand { Description = "test", Name = "" } };
+            yield return new object[] { new CreateAmenityCommand 
+                { 
+                    Description = new string('a',161),
+                    Name = new string('a',61) 
+                } 
+            };
         }
 
-        public static IEnumerable<object?[]> GetUpdateAmenityCommandTestData()
+        public static IEnumerable<object?[]> UpdateAmenityTestData()
         {
             yield return new object[] { new UpdateAmenityCommand { Name = "test" } };
             yield return new object[] { new UpdateAmenityCommand { Description = "test" } };
-            yield return new object[] { new UpdateAmenityCommand { Description = "test" } };
+            yield return new object[] { new UpdateAmenityCommand { Description = "", Name = "test" } };
+            yield return new object[] { new UpdateAmenityCommand { Description = "test", Name = "" } };
+            yield return new object[] { new UpdateAmenityCommand 
+                { 
+                    Description = new string('a', 161), 
+                    Name = new string('a', 61) 
+                } 
+            };
         }
 
-        public static IEnumerable<object[]> GetNotFoundTestData()
+        public static IEnumerable<object[]> NotFoundTestData()
         {
             yield return new object[] { "wrongId" };
             yield return new object[] { "-2" };
