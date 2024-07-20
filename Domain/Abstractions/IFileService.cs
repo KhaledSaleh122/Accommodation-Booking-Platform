16791a8fa44bd10f10d696a112b92a65a4d6697f
khaled Saleh <voxagolde2003@gmail.com>
2024-07-20 11:23:31 +0300

    Create interface to save images on the server

---
+++ Domain/Abstractions/IFileService.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+namespace Domain.Abstractions
+{
+    public interface IFileService
+    {
+        public string UploadFile(IFormFile formFile, string fileStorePath,string fileName);
+
+        public bool ValidateFile(IFormFile formFile);
+        public void DeleteFile(string filePath);
+    }
+}
