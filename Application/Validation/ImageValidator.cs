7502d0fa9b6d65a14b49d4702b63f61f6bc423b1
khaled Saleh <voxagolde2003@gmail.com>
2024-07-20 11:48:10 +0300

    Create image validator to vaild image vefore upload

---
+++ Application/Validation/ImageValidator.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Application.CommandsAndQueries.HotelCQ.Commands.Create;
+using Domain.Abstractions;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Validation
+{
+    public static class ImageValidator
+    {
+        public static void ValidateImage<TRequest>(
+                this IImageService imageRepository,
+                IFormFile image,
+                ValidationContext<TRequest> context,
+                string propertyName
+            )
+        {
+            try
+            {
+                imageRepository.ValidateFile(image);
+            }
+            catch (Exception ex)
+            {
+                context.AddFailure(propertyName, ex.Message);
+            }
+        }
+    }
+}
