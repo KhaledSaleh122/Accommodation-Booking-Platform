7a556c2b6329837863589d12c3234febfef3f242
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 13:14:08 +0300

    Generate migration to build room table

---
+++ Infrastructure/Migrations/20240721101158_CreateRoomTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateRoomTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    RoomNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    HotelId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    AdultCapacity = table.Column<int>(type: "int", nullable: false),
+                    ChildrenCapacity = table.Column<int>(type: "int", nullable: false),
+                    Thumbnail = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.CheckConstraint("CK_Rooms_AdultCapacity_gre_0", "AdultCapacity >= 0");
+                    table.CheckConstraint("CK_Rooms_ChildrenCapacity_gre_0", "ChildrenCapacity >= 0");
+                    table.PrimaryKey("PK_Room", x => new { x.HotelId, x.RoomNumber });
+                    table.ForeignKey(
+                        name: "FK_Room_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Room");
+        }
+    }
+}
