03f3a46ab6019fc79a8d30593fb9bf711e7a0bbf
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 13:16:33 +0300

    Create room images entity

---
+++ Domain/Entities/RoomImage.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class RoomImage
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string RoomNumber { get; set; }
+        public int HotelId { get; set; }
+        public Room room { get; set; }
+    }
+}
