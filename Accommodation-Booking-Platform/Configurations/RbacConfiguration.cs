8d03c4609b797e7f31eabdb27fd6274d29a535cb
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 23:20:58 +0300

    Create default admin account, and ensure create required roles

--- Accommodation-Booking-Platform/Configurations/RbacConfiguration.cs
+++ Accommodation-Booking-Platform/Configurations/RbacConfiguration.cs
@@ -6,11 +6,17 @@ namespace Booking_API_Project.Configurations
     public static class RbacConfiguration
     {
         public async static Task<IServiceProvider> AddRoleBasedAccessControl(
-            this IServiceProvider services)
+            this IServiceProvider services, IConfiguration configuration)
         {
             var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
             var userManager = services.GetRequiredService<UserManager<User>>();
             await EnsureRolesAsync(roleManager);
+            var adminName = configuration.GetValue<string>("Admin:Name");
+            var adminEmail = configuration.GetValue<string>("Admin:Email");
+            var adminPassword = configuration.GetValue<string>("Admin:Password");
+            if (adminName is null || adminEmail is null || adminPassword is null)
+                throw new Exception("Admin account information is required");
+            await CreateAdminAccount(userManager, adminName, adminEmail, adminPassword);
             return services;
         }
 
@@ -23,6 +29,24 @@ namespace Booking_API_Project.Configurations
                 {
                     await roleManager.CreateAsync(new IdentityRole(role));
                 }
+            }
+        }
+
+        private static async Task CreateAdminAccount(
+            UserManager<User> userManager,
+            string name,
+            string email,
+            string password
+            ) {
+            var user = await userManager.FindByNameAsync(name);
+            var user_ = await userManager.FindByEmailAsync(email);
+            if (user is null && user_ is not null)
+                throw new Exception("There is account with this email");
+            if (user is null)
+            {
+                user = new User { UserName = name, Email = email };
+                await userManager.CreateAsync(user, password);
+                await userManager.AddToRoleAsync(user, "Admin");
             }
         }
 
