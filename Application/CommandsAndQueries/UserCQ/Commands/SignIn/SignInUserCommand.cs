1b030c50c2bccb7d8ef9ef80ea34a3423a144940
khaled Saleh <voxagolde2003@gmail.com>
2024-07-22 00:22:39 +0300

    Implement signin user command

---
+++ Application/CommandsAndQueries/UserCQ/Commands/SignIn/SignInUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Dtos.UserDtos;
+using MediatR;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.CommandsAndQueries.UserCQ.Commands.SignIn
+{
+    public class SignInUserCommand : IRequest<string>
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Application/CommandsAndQueries/UserCQ/Commands/SignIn/SignInUserValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System.Data;
+
+namespace Application.CommandsAndQueries.UserCQ.Commands.SignIn
+{
+    public class SignInUserValidation : AbstractValidator<SignInUserCommand>
+    {
+        public SignInUserValidation() {
+            RuleFor(user => user.Password)
+                .NotEmpty();
+            RuleFor(user => user.UserName)
+                .NotEmpty();
+        }
+    }
+}
