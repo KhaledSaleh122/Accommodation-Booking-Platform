f6faf497dc3d3a7c9f1706f0a992a303fec8ad18
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 17:38:09 +0300

    Implement create user command

---
+++ Application/CommandsAndQueries/UserCQ/Commands/Create/CreateUserHandler.cs
@@ -0,0 +1,40 @@
+﻿using Application.Dtos.UserDtos;
+using Application.Execptions;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Application.CommandsAndQueries.UserCQ.Commands.Create
+{
+    public class CreateUserHandler : IRequestHandler<CreateUserCommand, UserDto>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+        public CreateUserHandler(UserManager<User> userManager)
+        {
+            this._userManager = userManager;
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserDto>();
+            });
+            _mapper = configuration.CreateMapper();
+        }
+        public async Task<UserDto> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+
+            var user = new User()
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (!result.Succeeded) {
+                throw new ErrorException($"Error during creating new User.");
+            }
+
+            return _mapper.Map<UserDto>(user);
+        }
+    }
+}
