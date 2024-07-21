054599876cf93c36d8e4b7641c209452f46f980d
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 13:29:18 +0300

    Generate migration to create room images table

---
+++ Infrastructure/Migrations/20240721102641_CreateRoomImagesTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateRoomImagesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RoomImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoomNumber = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    HotelId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoomImage_Room_HotelId_RoomNumber",
+                        columns: x => new { x.HotelId, x.RoomNumber },
+                        principalTable: "Room",
+                        principalColumns: new[] { "HotelId", "RoomNumber" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomImage_HotelId_RoomNumber",
+                table: "RoomImage",
+                columns: new[] { "HotelId", "RoomNumber" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomImage");
+        }
+    }
+}
