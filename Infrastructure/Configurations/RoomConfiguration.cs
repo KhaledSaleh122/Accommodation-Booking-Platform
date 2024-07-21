3549df254202f42499fd859a62452c211fc7c0ce
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 13:10:52 +0300

    Create room configuration

---
+++ Infrastructure/Configurations/RoomConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    internal class RoomConfiguration : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.Property(Room => Room.RoomNumber).HasMaxLength(10);
+            builder.HasKey(pro => new { pro.HotelId, pro.RoomNumber });
+            builder.HasOne(o => o.Hotel)
+                   .WithMany(o => o.Rooms)
+                   .HasForeignKey(o => o.HotelId);
+        }
+    }
+}
