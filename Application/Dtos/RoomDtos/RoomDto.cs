f9a818e772f07f99bce8e23ea0e9ca6608427e1c
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 13:39:02 +0300

    Implement room dto

---
+++ Application/Dtos/RoomDtos/RoomDto.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+
+namespace Application.Dtos.RoomDtos
+{
+    public class RoomDto
+    {
+        public string RoomNumber { get; set; }
+        public string Status { get; set; }
+        public int AdultCapacity { get; set; }
+        public int ChildrenCapacity { get; set; }
+        public string Thumbnail { get; set; }
+        public List<string> Images { get; set; }
+    }
+}
