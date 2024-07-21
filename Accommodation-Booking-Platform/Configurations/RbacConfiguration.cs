85bcc79af19ac9bec2c23cc36200d4bbf4995f65
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 17:54:31 +0300

    Add required configuration

---
+++ Accommodation-Booking-Platform/Configurations/RbacConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Booking_API_Project.Configurations
+{
+    public static class RbacConfiguration
+    {
+        public async static Task<IServiceProvider> AddRoleBasedAccessControl(
+            this IServiceProvider services)
+        {
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+            await EnsureRolesAsync(roleManager);
+            return services;
+        }
+
+       private static async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            var roles = new[] { "Admin", "User" };
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+
+    }
+}
