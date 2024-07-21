9142af70b7874dab7608c7e408d1f8ebbbf741c1
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 12:38:29 +0300

    Implement create and delete hotel amenity

---
+++ Application/CommandsAndQueries/HotelAmenityCQ/Commands/Delete/RemoveAmenityFromHotelCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Application.CommandsAndQueries.HotelAmenityCQ.Commands.Delete
+{
+    public class RemoveAmenityFromHotelCommand : IRequest
+    {
+        public RemoveAmenityFromHotelCommand()
+        {
+        }
+
+        public RemoveAmenityFromHotelCommand(int hotelId, int amenityId)
+        {
+            HotelId = hotelId;
+            AmenityId = amenityId;
+        }
+
+        public int HotelId { get; set; }
+        public int AmenityId { get; set; }
+    }
+}
