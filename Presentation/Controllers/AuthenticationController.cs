6e779d525a01910d61409008418ea9f7e376498c
khaled Saleh <voxagolde2003@gmail.com>
2024-07-22 00:43:13 +0300

    Implement signin action

--- Presentation/Controllers/AuthenticationController.cs
+++ Presentation/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.CommandsAndQueries.UserCQ.Commands.Create;
+using Application.CommandsAndQueries.UserCQ.Commands.SignIn;
 using Application.Dtos.UserDtos;
 using Application.Exceptions;
 using MediatR;
@@ -11,7 +12,7 @@ namespace Presentation.Controllers
 {
     [ApiController]
     [Route("api")]
-    public class AuthenticationController
+    public class AuthenticationController : ControllerBase
     {
         private readonly IMediator _mediator;
         private readonly ILogger<AmenityController> _logger;
@@ -24,12 +25,34 @@ namespace Presentation.Controllers
         [HttpPost("users")]
         [ProducesResponseType(typeof(UserDto), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(ValidationFailureResponse), StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> RegisterUser(CreateUserCommand? command) { 
-            if(command is null) throw new CustomValidationException("The body for this request required");
+        public async Task<IActionResult> RegisterUser(CreateUserCommand? command)
+        {
+            if (command is null) throw new CustomValidationException("The body for this request required");
             var createdUser = await _mediator.Send(command);
-            return new ObjectResult(createdUser) { 
-                StatusCode = StatusCodes.Status201Created 
+            _logger.LogInformation(
+                "New Account Created name '{command.UserName}',  Email '{command.Email}'",
+                command.UserName,
+                command.Email
+            );
+            return new ObjectResult(createdUser)
+            {
+                StatusCode = StatusCodes.Status201Created
             };
         }
+        [HttpPost("sessions")]
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationFailureResponse), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> LoginInUser(SignInUserCommand? command)
+        {
+            if (command is null) 
+                throw new CustomValidationException("The body for this request required");
+            var token = await _mediator.Send(command);
+            _logger.LogInformation(
+                "New login to account name '{command.UserName}'",
+                command.UserName
+            );
+            return Ok(token);
+        }
+
     }
 }
