1b730f734358e46188ae8d51800ff0b5b135ea08
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 12:48:19 +0300

    Implement add and remove amenity to/from hotel actions

---
+++ Presentation/Controllers/HotelAmenityController.cs
@@ -0,0 +1,54 @@
+﻿using Application.CommandsAndQueries.HotelAmenityCQ.Commands.Create;
+using Application.CommandsAndQueries.HotelAmenityCQ.Commands.Delete;
+using Application.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Presentation.Responses.NotFound;
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/hotels/{hotelId}/amenities")]
+    public class HotelAmenityController : ControllerBase
+    {
+        private readonly ILogger<HotelAmenityController> _logger;
+        private readonly IMediator _mediator;
+        public HotelAmenityController(ILogger<HotelAmenityController> logger, IMediator mediator)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost("{amenityId}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(NotFoundResponse), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> AddAmenityToHotel(int hotelId, int amenityId)
+        {
+            if (hotelId <= 0) throw new NotFoundException("Hotel not found!");
+            if (amenityId <= 0) throw new NotFoundException("Amenity not found!");
+            var addAmenityCommand = new AddAmenityToHotelCommand(hotelId, amenityId);
+            await _mediator.Send(addAmenityCommand);
+            _logger.LogInformation("Amenity with id '{amenityId}' added to hotel with id '{hotelId}'.", amenityId, hotelId);
+
+            return CreatedAtRoute("GetHotelById", new { hotelId }, null);
+        }
+
+        [HttpDelete("{amenityId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(NotFoundResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> RemoveAmenityFromHotel(int hotelId, int amenityId)
+        {
+            if (hotelId <= 0) throw new NotFoundException("Hotel not found!");
+            if (amenityId <= 0) throw new NotFoundException("Amenity not found!");
+            var addAmenityCommand = new RemoveAmenityFromHotelCommand(hotelId, amenityId);
+            await _mediator.Send(addAmenityCommand);
+            _logger.LogInformation(
+                "Amenity with id '{amenityId}' removed from hotel with id '{hotelId}'.", 
+                amenityId,
+                hotelId
+            );
+            return Ok();
+        }
+    }
+}
