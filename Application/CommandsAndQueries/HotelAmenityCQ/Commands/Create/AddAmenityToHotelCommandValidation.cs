285c74e4b4c7a1b1955b7d1196df41cbab0b9a31
khaled Saleh <voxagolde2003@gmail.com>
2024-07-21 12:38:29 +0300

    Implement create and delete hotel amenity

---
+++ Application/CommandsAndQueries/HotelAmenityCQ/Commands/Create/AddAmenityToHotelCommandValidation.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Abstractions;
+using FluentValidation;
+
+namespace Application.CommandsAndQueries.HotelAmenityCQ.Commands.Create
+{
+    public class AddAmenityToHotelCommandValidation : AbstractValidator<AddAmenityToHotelCommand>
+    {
+        private readonly IHotelRepository _hotelRepository;
+        public AddAmenityToHotelCommandValidation(IHotelRepository hotelRepository)
+        {
+            _hotelRepository = hotelRepository ?? throw new ArgumentNullException(nameof(hotelRepository));
+            RuleFor(m => m).CustomAsync
+                (
+                    async (command, context, token) =>
+                    {
+                        var isExist = await _hotelRepository.AmenityExistsAsync(command.HotelId, command.AmenityId);
+                        if (isExist)
+                        {
+                            context.AddFailure("AmenityId", "The hotel already has this amenity.");
+                        }
+                    }
+                );
+        }
+    }
+}
