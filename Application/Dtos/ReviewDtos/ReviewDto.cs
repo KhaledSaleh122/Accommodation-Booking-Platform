5fe1417674f0e4f80391ee7f14bec52451ee4f83
khaled Saleh <voxagolde2003@gmail.com>
2024-07-22 22:29:55 +0300

    Create review dto

---
+++ Application/Dtos/ReviewDtos/ReviewDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Dtos.ReviewDtos
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public decimal Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
