fd4a4bec1a07eded53f6f06cd47587aef6ac063f
khaled Saleh <voxagolde2003@gmail.com>
2024-07-22 23:00:14 +0300

    Implement create review command

---
+++ Application/CommandsAndQueries/ReviewCQ/Commands/CreateReviewValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.CommandsAndQueries.ReviewCQ.Commands
+{
+    public class CreateReviewValidation : AbstractValidator<CreateReviewCommand>
+    {
+        public CreateReviewValidation() {
+            RuleFor(review => review.Rating)
+                .NotEmpty()
+                .ExclusiveBetween(1,5);
+        }
+    }
+}
