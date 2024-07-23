9fa513339c2b3bc74b142b6284fa5a4185ff7d35
khaled Saleh <voxagolde2003@gmail.com>
2024-07-23 14:06:50 +0300

    Implement recently visited hotel repository

---
+++ Infrastructure/Repositories/RecentlyVisitedHotelRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public class RecentlyVisitedHotelRepository : IRecentlyVisitedHotelRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RecentlyVisitedHotelRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task AddAsync(RecentlyVisitedHotel recentlyVisitedHotel)
+        {
+            await _dbContext.RecentlyVisitedHotels.AddAsync(recentlyVisitedHotel);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
