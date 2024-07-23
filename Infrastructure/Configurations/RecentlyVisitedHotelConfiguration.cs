31c14546ec1f1fce3de5db8e317db878ba00bf30
khaled Saleh <voxagolde2003@gmail.com>
2024-07-23 13:56:13 +0300

    Add configuration on the recently visited hotel entity

---
+++ Infrastructure/Configurations/RecentlyVisitedHotelConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    public class RecentlyVisitedHotelConfiguration : IEntityTypeConfiguration<RecentlyVisitedHotel>
+    {
+        public void Configure(EntityTypeBuilder<RecentlyVisitedHotel> builder)
+        {
+            builder.HasKey(rvh => new { rvh.HotelId, rvh.UserId });
+            builder.Property(rvh => rvh.VisitedDate)
+                .IsRequired(true);
+        }
+    }
+}
