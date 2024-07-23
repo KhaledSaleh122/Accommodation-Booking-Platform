e0495710bf88930a715d4741e327818fa50ccecf
khaled Saleh <voxagolde2003@gmail.com>
2024-07-23 13:56:57 +0300

    Generate migration to build recently visited hotels table

---
+++ Infrastructure/Migrations/20240723105221_CreateRecentlyVisitedHotelsTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateRecentlyVisitedHotelsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RecentlyVisitedHotels",
+                columns: table => new
+                {
+                    HotelId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    VisitedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecentlyVisitedHotels", x => new { x.HotelId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_RecentlyVisitedHotels_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RecentlyVisitedHotels_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecentlyVisitedHotels_UserId",
+                table: "RecentlyVisitedHotels",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecentlyVisitedHotels");
+        }
+    }
+}
