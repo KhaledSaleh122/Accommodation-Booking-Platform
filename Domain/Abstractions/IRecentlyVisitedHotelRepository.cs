9593da381edc319207a65cc22731dc7c482dacd9
khaled Saleh <voxagolde2003@gmail.com>
2024-07-23 14:06:50 +0300

    Implement recently visited hotel repository

---
+++ Domain/Abstractions/IRecentlyVisitedHotelRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Abstractions
+{
+    public interface IRecentlyVisitedHotelRepository
+    {
+        public Task AddAsync(RecentlyVisitedHotel recentlyVisitedHotel);
+    }
+}
