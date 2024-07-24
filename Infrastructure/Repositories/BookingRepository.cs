09eeb836f45295ea09bc04ab633013b32d6398d3
khaled Saleh <voxagolde2003@gmail.com>
2024-07-24 16:40:31 +0300

    Implement Booking repository

---
+++ Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public BookingRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<Booking> CreateRoomBookingAsync(Booking booking)
+        {
+            await _dbContext.Bookings.AddAsync(booking);
+            await _dbContext.SaveChangesAsync();
+            return booking;
+        }
+    }
+}
