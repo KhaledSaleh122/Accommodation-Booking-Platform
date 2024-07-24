f1af723ec60cb3db2a82b093425825b890b9cc26
khaled Saleh <voxagolde2003@gmail.com>
2024-07-24 12:04:23 +0300

    Create booking entity

---
+++ Domain/Entities/Booking.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public sealed class Booking
+    {
+        #nullable disable
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string RoomNumber { get; set; }
+        public int HotelId { get; set; }
+        public Room Room { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
