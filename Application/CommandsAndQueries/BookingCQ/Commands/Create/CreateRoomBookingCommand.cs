ed45e2802553cdc9be06e1dc2a7f0916868a0c35
khaled Saleh <voxagolde2003@gmail.com>
2024-07-26 13:46:08 +0300

    Redesgin booking to support reserve multiple rooms

--- Application/CommandsAndQueries/BookingCQ/Commands/Create/CreateRoomBookingCommand.cs
+++ Application/CommandsAndQueries/BookingCQ/Commands/Create/CreateRoomBookingCommand.cs
@@ -8,12 +8,8 @@ namespace Application.CommandsAndQueries.BookingCQ.Commands.Create
 {
     public class CreateRoomBookingCommand : IRequest<BookingDto>
     {
-        public int hotelId;
-        public string roomNumber;
+        public int HotelId { get; set; }
         public string userId;
-        [Required]
-        public DateOnly StartDate { get; set; }
-        [Required]
-        public DateOnly EndDate { get; set; }
+        public CreateBookingDto[] RoomBookings { get; set; }
     }
 }
