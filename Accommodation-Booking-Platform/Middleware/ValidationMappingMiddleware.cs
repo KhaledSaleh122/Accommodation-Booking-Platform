8a2d496a418421f08b008bec8314b312a2583114
khaled Saleh <voxagolde2003@gmail.com>
2024-07-28 13:56:36 +0300

    Include inner exception message

--- Accommodation-Booking-Platform/Middleware/ValidationMappingMiddleware.cs
+++ Accommodation-Booking-Platform/Middleware/ValidationMappingMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Exceptions;
 using FluentValidation;
 using Presentation.Responses.Validation;
+using System.Text.Json;
 
 namespace Accommodation_Booking_Platform.Middleware
 {
@@ -18,15 +19,20 @@ namespace Accommodation_Booking_Platform.Middleware
             {
                 await _next(context);
             }
-            catch (CustomValidationException exception) {
+            catch (CustomValidationException exception)
+            {
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                var validationFailureResponse = new ValidationFailureResponse() { 
-                    Errors = null,
+                var validationFailureResponse = new ValidationFailureResponse()
+                {
                     Title = exception.Message,
                     Status = StatusCodes.Status400BadRequest,
                     TraceId = context.TraceIdentifier
                 };
-                await context.Response.WriteAsJsonAsync(validationFailureResponse);
+                var options = new JsonSerializerOptions
+                {
+                    DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+                };
+                await context.Response.WriteAsJsonAsync(validationFailureResponse, options);
             }
             catch (ValidationException exception)
             {
